c9362dbbf79be27c3c09945673ce1cc79ccd092e
Smoualouks <omari.ismail@gmail.com>
2022-08-29 17:42:25 +0200

    commit Angular 29/08

--- src/app/shared/services/space.service.ts
+++ src/app/shared/services/space.service.ts
@@ -2,15 +2,16 @@ import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
 import { HttpClient } from '@angular/common/http';
 import { Space } from '../interfaces/space';
+import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class SpaceService {
-  private _baseUrl = environment.urlApi + '/spaces';
+  private _baseUrl = environment.urlApi + '/space/limit';
   spaces$: any;
 
-  constructor(private _http: HttpClient) { }
+  constructor(private _http: HttpClient) {}
   public findAll(){
     return this._http.get<Space[]>(this._baseUrl);
   }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: false,
+  urlApi: 'http://localhost:8080/rest',
 };
